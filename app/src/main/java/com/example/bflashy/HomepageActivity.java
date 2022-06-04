294e829e9fe4328b14108c05a015ddb44516d0ec
Kritik Modi <kritikmodi@gmail.com>
2022-06-04 13:30:06 +0530

    Add bottom-navigator and homepage fragments

--- app/src/main/java/com/example/bflashy/HomepageActivity.java
+++ app/src/main/java/com/example/bflashy/HomepageActivity.java
@@ -1,19 +1,86 @@
 package com.example.bflashy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomepageActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    String myuid;
+    ActionBar actionBar;
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getSupportActionBar().hide();
-
         setContentView(R.layout.activity_homepage);
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Profile Activity");
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(selectedListener);
+        actionBar.setTitle("Home");
+
+        // When we open the application first
+        // time the fragment should be shown to the user
+        // in this case it is home fragment
+        HomeFragment fragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content, fragment, "");
+        fragmentTransaction.commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+
+                case R.id.nav_home:
+                    actionBar.setTitle("Home");
+                    HomeFragment fragment1 = new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content, fragment1, "");
+                    fragmentTransaction.commit();
+                    return true;
+
+                case R.id.nav_mint:
+                    actionBar.setTitle("Mint a new POAP");
+                    MintFragment fragment2 = new MintFragment();
+                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.content, fragment2, "");
+                    fragmentTransaction2.commit();
+                    return true;
+
+                case R.id.nav_show:
+                    actionBar.setTitle("Your POAP collection");
+                    CollectionsFragment fragment3 = new CollectionsFragment();
+                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction3.replace(R.id.content, fragment3, "");
+                    fragmentTransaction3.commit();
+                    return true;
+
+                case R.id.nav_profile:
+                    actionBar.setTitle("Your Profile");
+                    ProfileFragment fragment4 = new ProfileFragment();
+                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction1.replace(R.id.content, fragment4);
+                    fragmentTransaction1.commit();
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
 }
